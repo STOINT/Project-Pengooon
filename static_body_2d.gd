7da158bdda2f6e375d2a30558224d53ccae1aba5
STOINTY <20171@ROSMINI.SCHOOL.NZ>
2025-03-24 15:08:19 +1300

    yes

---
+++ static_body_2d.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+animated_sprite_2d.play("SprintLeft")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
