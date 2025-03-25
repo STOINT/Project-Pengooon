fa9116aa89ddfa119d1f89c7d77b0b0fd85e3067
STOINTY <20171@ROSMINI.SCHOOL.NZ>
2025-03-26 09:43:21 +1300

    Fixed after merger

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,7 +3,6 @@ extends Control
 
 func _on_texture_button_pressed():
 	get_tree().change_scene_to_file("res://Levels/Level1.tscn")
-	print("fatty worky pweaseeeea")
 
 
 func _on_texture_button_mouse_entered():
