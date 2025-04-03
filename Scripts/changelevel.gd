c47684eb90e5bebf48f4bd7c91ab727a73c07b95
STOINTY <20171@ROSMINI.SCHOOL.NZ>
2025-04-03 22:05:50 +1300

    Reapply "Changed"
    This reverts commit 04c8bb445d584f69c4c5a2697e191aec7ae2e75e.

--- Scripts/changelevel.gd
+++ Scripts/changelevel.gd
@@ -1,5 +1,6 @@
 extends Node
 
+<<<<<<< Updated upstream:Scripts/changelevel.gd
 
 # Called when the node enters the scene tree for the first time.
 
@@ -10,3 +11,7 @@ extends Node
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+=======
+func _ready():
+	animated_sprite_2d.play("ShrimpBob")
+>>>>>>> Stashed changes:Level Builder/node_2d.gd
