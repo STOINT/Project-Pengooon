300c1bd79c2544e249802d5b7d100352d1c7fd26
STOINTY <20171@ROSMINI.SCHOOL.NZ>
2025-04-28 01:18:21 +1200

    Final Change

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -81,4 +81,4 @@ func _on_portal_2_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://Levels/Level3.tscn")
 
 func _on_portal_3_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://Levels/WinScreen.tscn")
+	get_tree().change_scene_to_file("res://End SCreen.tscn")
