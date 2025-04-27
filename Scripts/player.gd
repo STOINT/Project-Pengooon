ddc4a2ab62598fa9de22ecc069c31f93b4a03bee
STOINTY <20171@ROSMINI.SCHOOL.NZ>
2025-04-28 01:13:52 +1200

    Final Changes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -75,7 +75,7 @@ func _physics_process(delta):
 
 
 func _on_portal_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://Levels/Level2.tscn")
+	get_tree().change_scene_to_file("res://Character/Level2.tscn")
 
 func _on_portal_2_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://Levels/Level3.tscn")
