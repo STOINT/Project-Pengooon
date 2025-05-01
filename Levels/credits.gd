291b95261d2ff8748d307e50ffa7c25007de79d1
20036R <161525710+20036R@users.noreply.github.com>
2025-05-01 20:02:53 +1200

    Credit Scene rework
    Made links clickable awaiting for blue penguin information

---
+++ Levels/credits.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+func _on_background_pressed() -> void:
+	OS.shell_open("https://www.artstation.com/artwork/wJDq1g")
+
+func _on_penguin_pressed() -> void:
+	OS.shell_open("https://caz-creates-games.itch.io/lil-penguin-character-sprite")
+
+func _on_tilemap_pressed() -> void:
+	OS.shell_open("https://kidscancode.org/godot_recipes/3.x/2d/autotile_intro/index.html")
+
+func _on_tilemaptemplate_pressed() -> void:
+	OS.shell_open("https://www.gameart2d.com/winter-platformer-game-tileset.html")
